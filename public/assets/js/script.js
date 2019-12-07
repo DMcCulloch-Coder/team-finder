e51b31e262bd92b7fd2ce62c7b02efddd664412b
David <DMcCulloch86@gmail.com>
2019-12-06 20:43:56 -0500

    connected form info to script.js

---
+++ public/assets/js/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+
+    $('#submit').on('click', (event) => {
+        event.preventDefault()
+        console.log($('#name').val())
+        console.log($('#email').val())
+        console.log($('#photo').val())
+        console.log($('#question-1').val())
+        console.log($('#question-2').val())
+        console.log($('#question-3').val())
+        console.log($('#question-4').val())
+        console.log($('#question-5').val())
+        console.log($('#question-6').val())
+        console.log($('#question-7').val())
+        console.log($('#question-8').val())
+        console.log($('#question-9').val())
+        console.log($('#question-10').val())
+        
+    })
+
+
+
+})
+    
--- src/server.js
+++ src/server.js
@@ -11,6 +11,11 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 // Setup static directory to server
 app.use(express.static(publicDirectoryPath));
 
+// Catch all undefined routes to index.html
+app.get('*', (req, res) => {
+    res.redirect('/')
+})
+
 // Listener for port
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`)
